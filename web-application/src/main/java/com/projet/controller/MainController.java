baedb41268552fad8e65db424275a926f31395d2
Alex-Serky <aserky@gmail.com>
2020-10-18 18:49:37 +0200

    L'injection de dépendance ou Dependency Injection

--- web-application/src/main/java/com/projet/controller/MainController.java
+++ web-application/src/main/java/com/projet/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.projet.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,8 @@ import com.projet.web.ValidPassword;
 @Controller
 public class MainController {
 	
-	ValidPassword validPassword = new ValidPassword();
+	@Autowired
+	ValidPassword validPassword;
 
 	@RequestMapping("/")
 	public String pageAccueil() {
--- web-application/src/main/java/com/projet/web/ValidPassword.java
+++ web-application/src/main/java/com/projet/web/ValidPassword.java
@@ -1,5 +1,8 @@
 package com.projet.web;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ValidPassword {
 	public boolean estValide(String prenom, String password) {
 		if (prenom.equals("Alex") && password.equals("admin")) {
